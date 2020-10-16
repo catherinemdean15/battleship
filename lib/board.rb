4dc0b0d47da688f5ccdb202a4b47cd7fd0e9d9b5
Isabelle Villasenor <villasenori@outlook.com>
2020-10-15 21:20:06 -0600

    New init, board_size, and cells method.

--- lib/board.rb
+++ lib/board.rb
@@ -5,35 +5,17 @@ class Board
   def initialize
     @cells = {}
     @horizontal = (1..4).to_a
-    @vertical = ("A".."D")
-    #    "A1" => Cell.new("A1"),
-    #    "A2" => Cell.new("A2"),
-    #    "A3" => Cell.new("A3"),
-    #    "A4" => Cell.new("A4"),
-    #    "B1" => Cell.new("B1"),
-    #    "B2" => Cell.new("B2"),
-    #    "B3" => Cell.new("B3"),
-    #    "B4" => Cell.new("B4"),
-    #    "C1" => Cell.new("C1"),
-    #    "C2" => Cell.new("C2"),
-    #    "C3" => Cell.new("C3"),
-    #    "C4" => Cell.new("C4"),
-    #    "D1" => Cell.new("D1"),
-    #    "D2" => Cell.new("D2"),
-    #    "D3" => Cell.new("D3"),
-    #    "D4" => Cell.new("D4")
-    # }
+    @vertical = ("A".."D").to_a
+    @board_layout = board_size
   end
 
   def board_size
-    # @horizontal = (1..4).to_a
-    # @vertical = ("A".."D").to_a
     coordinates = @vertical.map do |letter|
       @horizontal.map do |num|
         letter + num.to_s
       end
     end
-    coordinates.flatten
+    puts coordinates.to_s
   end
 
   def cells
