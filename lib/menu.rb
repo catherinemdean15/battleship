12499374a1e589513480a5b640ed13a39518ebef
Catherine Dean <catherinemdean15@gmail.com>
2020-10-20 17:32:42 -0600

    quick before I mess up the computer_placement!

--- lib/menu.rb
+++ lib/menu.rb
@@ -19,36 +19,71 @@ class Menu
     @cruiser_player= Ship.new("Cruiser", 3)
   end
 
-  def computer_ship_placement
-    letters = ["A", "B", "C","D"]
-    numbers = [1, 2, 3, 4]
-    placed_cells = 0
-    until placed_cells == 2
-      first_coordinate = "#{letters[rand(letters.count)]}#{numbers[rand(numbers.count)]}"
-      second_coordinate = "#{(first_coordinate[0])}#{(first_coordinate[1].to_i) + 1}"
-      if @board_computer.valid_placement?(@submarine_computer,[first_coordinate, second_coordinate] )
-        @board_computer.place(@submarine_computer, [first_coordinate, second_coordinate])
-        @board_computer.cells.values.each do |cell|
-          if !cell.empty?
-            placed_cells += 1
-          end
-        end
-      end
-      require 'pry'; binding.pry
-    end
-    until placed_cells == 5
-      first_coordinate = "#{letters[rand(letters.count)]}#{numbers[rand(numbers.count)]}"
-      @board_computer.place(@cruiser_computer, [first_coordinate,
-                                "#{(first_coordinate[0])}#{(first_coordinate[1].to_i) + 1}",
-                                "#{(first_coordinate[0])}#{(first_coordinate[1].to_i) + 2}"
-                                  ])
+  def computer_submarine_placement
+    empty_cells_num = 0
+    until empty_cells_num == (@board_computer.cells.count - 2)
+      # require 'pry';binding.pry
+    coordinate_1 = @board_computer.cells.keys[rand(@board_computer.cells.count)]
+    coordinate_2 = "#{coordinate_1[0]}#{(coordinate_1[1].to_i) +1}"
+    @board_computer.place(@submarine_computer, [coordinate_1, coordinate_2])
       @board_computer.cells.values.each do |cell|
-        if !cell.empty?
-          placed_cells += 1
+        if cell.empty?
+        empty_cells_num += 1
         end
       end
     end
   end
+
+
+  def computer_cruiser_placement
+    empty_cells_num = 0
+    until empty_cells_num == (@board_computer.cells.count - 5)
+    coordinate_1 = @board_computer.cells.keys[rand(@board_computer.cells.count)]
+    coordinate_2 = "#{coordinate_1[0]}#{(coordinate_1[1].to_i) +1}"
+    coordinate_3 = "#{coordinate_1[0]}#{(coordinate_1[1].to_i) +2}"
+      if @board_computer.valid_coordinate?(coordinate_3)
+        # require 'pry'; binding.pry
+        @board_computer.place(@cruiser_computer, [coordinate_1, coordinate_2, coordinate_3])
+      end
+      @board_computer.cells.values.each do |cell|
+        # require 'pry';binding.pry
+        if cell.empty?
+        empty_cells_num += 1
+        end
+      end
+    end
+  end
+  #   letters = ["A", "B", "C","D"]
+  #   numbers = [1, 2, 3, 4]
+  #   placed_cells = 0
+  #   until placed_cells == 2
+  #     first_coordinate = "#{letters[rand(letters.count)]}#{numbers[rand(numbers.count)]}"
+  #     second_coordinate = "#{(first_coordinate[0])}#{(first_coordinate[1].to_i) + 1}"
+  #     if @board_computer.valid_placement?(@submarine_computer,[first_coordinate, second_coordinate])
+  #       @board_computer.place(@submarine_computer, [first_coordinate, second_coordinate])
+  #       @board_computer.cells.values.each do |cell|
+  #         if !cell.empty?
+  #           placed_cells += 1
+  #         end
+  #       end
+  #     end
+  #   end
+  #   until placed_cells == 5
+  #     first_coordinate = "#{letters[rand(letters.count)]}#{numbers[rand(numbers.count)]}"
+  #     second_coordinate = "#{(first_coordinate[0])}#{(first_coordinate[1].to_i) + 1}"
+  #     third_coordinate = "#{(first_coordinate[0])}#{(first_coordinate[1].to_i) + 2}"
+  #     if @board_computer.valid_placement?(@cruiser_computer,[first_coordinate, second_coordinate, third_coordinate])
+  #       @board_computer.place(@cruiser_computer, [first_coordinate, second_coordinate, third_coordinate])
+  #       @board_computer.cells.values.each do |cell|
+  #         # require 'pry'; binding.pry
+  #         if !cell.empty?
+  #               placed_cells += 1
+  #         end
+  #       end
+  #     end
+  #   end
+  #   require 'pry'; binding.pry
+  # end
 
   def user_ship_placement
     puts "I have laid out my ships on the grid."
@@ -90,7 +125,8 @@ class Menu
       else
         puts "Are you sure about that?"
     end
-    computer_ship_placement
+    computer_submarine_placement
+    computer_cruiser_placement
     puts @board_computer.render(true)
     user_ship_placement
     puts @board_player.render(true)
