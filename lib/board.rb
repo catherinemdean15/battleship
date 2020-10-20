a20ac65d5d90b54329cd80ae76d205e971eb02a5
Catherine Dean <catherinemdean15@gmail.com>
2020-10-19 20:22:23 -0600

    add completed render method

--- lib/board.rb
+++ lib/board.rb
@@ -1,25 +1,19 @@
 class Board
   attr_reader :cells
   def initialize
-    @cells = {
-       "A1" => Cell.new("A1"),
-       "A2" => Cell.new("A2"),
-       "A3" => Cell.new("A3"),
-       "A4" => Cell.new("A4"),
-       "B1" => Cell.new("B1"),
-       "B2" => Cell.new("B2"),
-       "B3" => Cell.new("B3"),
-       "B4" => Cell.new("B4"),
-       "C1" => Cell.new("C1"),
-       "C2" => Cell.new("C2"),
-       "C3" => Cell.new("C3"),
-       "C4" => Cell.new("C4"),
-       "D1" => Cell.new("D1"),
-       "D2" => Cell.new("D2"),
-       "D3" => Cell.new("D3"),
-       "D4" => Cell.new("D4")
-    }
-  end
+      @cells = {}
+      create_board
+    end
+
+    def create_board
+      letters = ["A", "B", "C", "D"]
+      numbers = [1, 2, 3, 4]
+      letters.each do |letter|
+      numbers.each do |number|
+        @cells["#{letter}#{number}"] = Cell.new("#{letter}#{number}")
+      end
+      end
+    end
 
   def valid_coordinate?(coordinates)
     @cells[coordinates] != nil
@@ -72,45 +66,14 @@ class Board
       horizontal.each do |number|
         output_string += number.to_s + " "
       end
-      vertical.each do |letter|
-        require 'pry'; binding.pry
-        output_string += "\nA"
-        if @cells.values[0][0] == letter
-          @cells.values.each do |cell|
-            cell.render(true)
-            output_string += "#{cell.render(true)} "
-          end
-        end
-        output_string += "\nB"
-        if @cells.values[1][0] == letter
-          @cells.values.each do |cell|
-            cell.render(true)
-            output_string += "#{cell.render(true)} "
-          end
-        end
+    vertical.each do |letter|
+      output_string += "\n#{letter} "
+      horizontal.each do |number|
+          output_string += "#{@cells["#{letter}#{number}"].render(show)} "
       end
-      output_string
     end
-      #   output_string += "\n#{letter} . . . ."
+    output_string + "\n"
+  end
 
-      # @cells.values.each do |cell|
-      #   require 'pry'; binding.pry
-      #   output_string += "\nA "
-      #   4.times do |cell|
-      #     if
-      #     output_string += "#{cell.render(true)} "
-      #   end
-      #   output_string += "\nB "
-      #   4.times do |cell|
-      #     output_string += "#{cell.render(true)} "
-      #   end
-          # horizontal.each do |number|
-          # coordinate = vertical[letter] + number.to_s
-    #       # output_string += @cells[coordinate].render(show_ships) + " "
-    #   end
-    #
-    #   output_string + " \n"
-    #   # require 'pry'; binding.pry
-    # end
 
 end
